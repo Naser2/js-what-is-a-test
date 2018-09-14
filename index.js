711770d5a9f70b95a231bc7039e8c17102cb23d3
Nasser Sanou <techman@Nassers-MacBook-Pro.local>
2018-09-14 18:46:32 -0400

    just in case

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
 console.log(message)
+
+
+var name = "Joe"
+var height = "74"
+var name = "Janet"
+
+
+
+console.log("Name:")
+console.log(name)
+
+console.log("Height:")
+console.log(height)
+console.log(parseInt(height) + 1)
 module.exports = {name, height, message}
